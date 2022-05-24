a54ff1f43ea36a61131f2ab9d5f3377887a11d84
toshiroto <chtopete@gmail.com>
2022-05-24 15:49:12 +0900

    First Intro Rendered

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,21 +1,12 @@
+import React from "react";
+import Intro from "./Intro";
+
 const App = () => {
   return (
-    <section id="intro">
-      <p class="name">
-        Hello, my name is
-        <span>Carlos.</span>
-      </p>
-       <h2>Front-end Developer</h2>
-      <p>
-        I'm a Front-end Web Developer with deep experiential knowledge in social
-        entrepreneurship and management
-      </p>
-      <p>I'm specialized in HTML, CSS, React, and Ruby on Rails</p>
-      <p>Currently I am working on a web-app directory to promote fair trade for{" "}
-        <a href="http://envia.org">Fundación En Vía</a>
-      </p>
-    </section>
-  );
-};
+    <div>
+      <Intro />
+    </div>
+  )
+}
 
 export default App;
---
+++ react-portfolio/src/Intro.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+
+  <div id="intro">
+      <p className="name">
+        Hello, my name is
+        <span>Carlos.</span>
+      </p>
+       <h2>Front-end Developer</h2>
+      <p>
+        I'm a Front-end Web Developer with deep experiential knowledge in social
+        entrepreneurship and management
+      </p>
+      <p>I'm specialized in HTML, CSS, React, and Ruby on Rails</p>
+      <p>Currently I am working on a web-app directory to promote fair trade for{" "}
+        <a href="http://envia.org">Fundación En Vía</a>
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
