c6191ac14ca04ccb179a14cc2219cf2c7b88cd7c
toshiroto <chtopete@gmail.com>
2022-05-24 15:17:54 +0900

    First Intro

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <section id="intro">
+      <p class="name">
+        Hello, my name is
+        <span>Carlos.</span>
+      </p>
+       <h2>Front-end Developer</h2>
+      <p>
+        I'm a Front-end Web Developer with deep experiential knowledge in social
+        entrepreneurship and management
+      </p>
+      <p>I'm specialized in HTML, CSS, React, and Ruby on Rails</p>
+      <p>Currently I am working on a web-app directory to promote fair trade for{" "}
+        <a href="http://envia.org">Fundación En Vía</a>
+      </p>
+    </section>
+  );
+};
+
+export default App;
