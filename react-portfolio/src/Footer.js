32ceb04ebc7b77312d8ed533fc0deebe98327eff
toshiroto <chtopete@gmail.com>
2022-05-27 12:27:58 +0900

    Fix footer HTML

--- react-portfolio/src/Footer.js
+++ react-portfolio/src/Footer.js
@@ -6,34 +6,30 @@ const Footer = () => {
   let year =  date.getFullYear()
   return (
     <footer>
-      <ul>
-        <li>
-          <h2>
-            Carlos Topete &middot; Front-end Developer
-          </h2>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/topete/">
-            <span
-               className="fa-brands fa-linkedin" aria-hidden="true"></span>
-            <span className="sr-only">LinkedIn</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/toshiroto">
-            <span
-              className="fa-brands fa-github-square" aria-hidden="true"></span>
-            <span className="sr-only">Github</span>
-          </a>
-        </li>
-        <li>
-          <a href="mailto:chtopete@gmail.com">
-            <span className="fa-solid fa-envelope" aria-hidden="true"></span>
-            <span className="sr-only">Email</span>
-          </a>
-        </li>
-        <p><small>&copy; {year} Carlos Topete. All rights reserved.</small></p>
+      <h2>Carlos Topete &middot; Front-end Developer </h2>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/topete/">
+              <span
+                className="fa-brands fa-linkedin" aria-hidden="true"></span>
+              <span className="sr-only">LinkedIn</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/toshiroto">
+              <span
+                className="fa-brands fa-github-square" aria-hidden="true"></span>
+              <span className="sr-only">Github</span>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:chtopete@gmail.com">
+              <span className="fa-solid fa-envelope" aria-hidden="true"></span>
+              <span className="sr-only">Email</span>
+            </a>
+          </li>
       </ul>
+        <p><small>&copy; {year} Carlos Topete. All rights reserved.</small></p>
     </footer>
   )
 }
