aa383bafe7d3ad72229cafa3ff1f9a827dc36b47
toshiroto <chtopete@gmail.com>
2022-06-08 13:29:43 +0900

    react rounter

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Projects from "./components/Projects"
+import Projects from "./components/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Intro />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/projects/" element={<Projects />} />
+          <Route path="/index.html" element={<Intro />} />
+        </Routes>
+        <Contact />
+        <Footer />
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
